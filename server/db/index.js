285418aae5b3c9cbc47d2de7bdd529ab8b5c2f98
onlytoseef <hellotoseef@gmail.com>
2025-01-02 11:00:24 +0500

    connected DB

---
+++ server/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+    console.log("DB Conencted ");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import { connectDB } from "./db/index.js";
 
 const app = express();
 const PORT = process.env.PORT;
+connectDB();
 
 app.listen(PORT, (req, res) => {
   console.log(
